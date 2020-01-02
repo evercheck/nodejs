81e3824481b2fdbc30e6fccd85b5abd2b274fefa
evercheck <evercheck@gmal.com>
2020-01-02 16:41:03 +0700

    Update repository with firedata

--- wacshop/functions/src/common/repository.ts
+++ wacshop/functions/src/common/repository.ts
@@ -1,16 +1,7 @@
-import { Injectable } from '@nestjs/common';
-import { Firestore } from '@google-cloud/firestore';
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
-@Injectable()
-export class Repository {
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
-    db: Firestore;
-
-    constructor() {
-        console.log('constructor firestore');
-        admin.initializeApp(functions.config().firebase);
-        this.db = admin.firestore();
-    }
-}
+export default db;
--- wacshop/functions/src/items/items.service.ts
+++ wacshop/functions/src/items/items.service.ts
@@ -1,27 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { Item } from './item.interface';
-import { Repository } from '../common/repository';
-import * as admin from 'firebase-admin';
-import * as functions from 'firebase-functions';
-import { Firestore } from '@google-cloud/firestore';
+import db from '../common/repository';
 
 @Injectable()
 export class ItemsService {
 
     private readonly items: Item[] = [];
-    private readonly db: Firestore;
-
-    constructor() {
-        admin.initializeApp(functions.config().firebase);
-        this.db = admin.firestore();
-    }
 
     findAll(): Item[] {
         return this.items;
     }
 
     create(item: Item) {
-        console.log('KHOA: CREATE ITEM');
-        this.db.collection('items').doc(item.name).set(item);
+        console.log('CREATE ITEM');
+        db.collection('items').doc(item.name).set(item);
     }
 }
